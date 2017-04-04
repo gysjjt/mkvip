923a534a713a59bf0be567865d0425afefd66983
hx <hx@qq.com>
2017-04-04 11:11:43 +0800

    ..

--- login.php
+++ login.php
@@ -38,7 +38,7 @@ if($_GET['action'] == "code"){//获取验证码
     //总页数
     $pages = ceil($totals/50);
 
-	for($i=1; $i<=1; $i++){
+	for($i=1; $i<=$pages; $i++){
 		$params = "p=$i&birthBegin=&birthEnd=&czCountE=&czCountS=&czE=&czS=&gender=-1&invalidDate=0&jfE=&jfS=&keyword=&kkBegin=&kkEnd=&lxfBegin=&lxfEnd=&mctype=0&mtype=0&notxfDate=0&ostate=0&othkw=&qkE=&qkS=&sortPreField=&sortd=&sortf=&xfBegin=&xfCountE=&xfCountS=&xfE=&xfEnd=&xfS=&xfitem=0&yueE=&yueS=&zjE=&zjS=";
 		$curl -> params = $params;
 		$curl -> url = "http://vip.minicon.net/iframepage/apppage/member_list.aspx";
